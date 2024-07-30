547d4db204995b9352d7470baa48bfb35bcdc61d
Praanth.Battula <Praanth.Battula@ext.dpworld.com>
2024-07-30 23:06:55 +0530

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,5 +8,22 @@ module.exports = {
   ],
   rules: {
     'no-console': 'error'
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
   }
 }
