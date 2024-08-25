178e388766e291b8c3e3c6ba6bec5cdc6aeff834
Praanth.Battula <Praanth.Battula@ext.dpworld.com>
2024-08-25 13:36:05 +0530

    added update and delete operations

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,22 @@
-import { createProject, getAllProjects, getProjectById } from './services/projects'
+import {
+  createProject,
+  deleteProject,
+  getAllProjects,
+  getProjectById,
+  updateProject
+} from './services/projects'
 import { v4 as uuidv4 } from 'uuid'
 
 function App(): JSX.Element {
   const onCreate = (): unknown => {
     createProject({ name: 'test-project', _id: uuidv4() })
     console.log('get project', getProjectById('b10e7c33-336e-4dfb-8fad-0ed43a702703'))
+    updateProject({
+      name: 'title update',
+      _rev: '1-462da7ef179101dacd44aae392553a57',
+      _id: 'b10e7c33-336e-4dfb-8fad-0ed43a702703'
+    })
+    deleteProject('b10e7c33-336e-4dfb-8fad-0ed43a702703')
     getAllProjects()
     return null
   }
--- src/renderer/src/services/projects.ts
+++ src/renderer/src/services/projects.ts
@@ -1,14 +1,26 @@
-import { CreateDocSuccessResponse } from '@renderer/types/common'
-import { CreateProjectReqest } from '@renderer/types/project'
+import { CreateDocSuccessResponse, GetMeta } from '@renderer/types/common'
+import { CreateProjectRequest, Project } from '@renderer/types/project'
 import PouchDB from 'pouchdb'
 
 const db = new PouchDB('podo_projects')
 
 // create a new project
-export const createProject = (data: CreateProjectReqest): unknown => {
+export const createProject = (data: CreateProjectRequest): unknown => {
   db.put(data)
     .then((response: CreateDocSuccessResponse) => {
       console.log(`Project created with id: ${response.id}`)
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+  return
+}
+
+// update project
+export const updateProject = (data: Project): unknown => {
+  db.put(data)
+    .then((response: CreateDocSuccessResponse) => {
+      console.log(`Project updated with id: ${response.id}`, response)
     })
     .catch(function (err) {
       console.log(err)
@@ -20,6 +32,7 @@ export const createProject = (data: CreateProjectReqest): unknown => {
 export const getProjectById = (id: string): unknown => {
   db.get(id)
     .then((response: unknown) => {
+      console.log(`Project retrieved with id: ${id}`, response)
       return response
     })
     .catch(function (err) {
@@ -31,7 +44,24 @@ export const getProjectById = (id: string): unknown => {
 // get all projects
 export const getAllProjects = (): unknown => {
   db.allDocs({ include_docs: true, attachments: true }).then((results: unknown) => {
+    console.log('All projects:', results)
     return results
   })
+  return
+}
+
+// delete project
+export const deleteProject = (id: string): unknown => {
+  db.get(id)
+    .then((doc: GetMeta) => {
+      return db.remove(doc)
+    })
+    .then((result: unknown) => {
+      console.log(`Project deleted with id: ${id}`, result)
+      // handle result
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
   return
 }
