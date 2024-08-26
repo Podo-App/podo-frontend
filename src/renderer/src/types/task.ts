854d1b28400715e1b6ee0f82826955f5560f7dee
prasanthbattula <prasanthbattula5@gmail.com>
2024-08-26 18:35:28 +0530

    added task type

---
+++ src/renderer/src/types/task.ts
@@ -0,0 +1,19 @@
+type Priority = 'Low' | 'Medium' | 'High'
+
+interface SubTask {
+  name: string
+  isCompleted: boolean
+}
+
+export interface Task {
+  name: string
+  description: string
+  dueDate: Date | string | number
+  priority: Priority
+  projectId: string
+  totalPomos: number
+  completedPomos: number
+  isActive: boolean
+  isCompleted: boolean
+  subTasks: SubTask[]
+}
